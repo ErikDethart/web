0671922121d3982f7388132a536c2d87f4fb44d4
Erik Dethart <mail@erikdethart.com>
2023-09-29 16:16:51 -0500

    generate library and app to develop it in

---
+++ libs/component-lib/src/lib/component-lib.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-component-lib',
+  template: `
+    <p>
+      component-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ComponentLibComponent {
+
+}
